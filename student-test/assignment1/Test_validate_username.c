db706e8dfba84b9e2fd1e07d69e4f2fa1afd4844
Jan Feemers <jan.feemers@siemens.com>
2023-11-06 21:57:55 +0100

    feat(username): add name for assignment

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,9 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    char const * const pMyUserName = my_username();
+    char const * const pMyUserNameConfFile = malloc_username_from_conf_file();
+    const int result = strcmp(pMyUserName, pMyUserNameConfFile);
+
+    TEST_ASSERT_EQUAL_STRING(pMyUserName, pMyUserNameConfFile);
 }
